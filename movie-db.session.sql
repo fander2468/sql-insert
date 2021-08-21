dd15a120c2e48622d16faf81f157a9e6db1e6052
fander2468 <86483561+fander2468@users.noreply.github.com>
2021-08-21 18:07:24 -0400

    first commit

---
+++ movie-db.session.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO concessions (drinks_food, amount_of_transactions)
+-- VALUES ('popcorn','20')
+
+-- INSERT INTO customers (customers_serviced, transactions)
+-- VALUES ('200','30000.00')
+
+-- INSERT INTO movies (movie_name)
+-- VALUES ('A very blue sky')
+
+-- INSERT INTO tickets (amount_of_tickets_sold)
+-- VALUES ('150')
